1e209c53153b1f61a8e122c4b2756a4e8fbb60ff
Kai Butt <kai@kaipuma.com>
2022-02-15 22:13:11 -0330

    moved config file to root until such a time as multiple config files are needed

--- bot.py
+++ bot.py
@@ -5,18 +5,18 @@ import disnake as snek
 from disnake.ext import commands as cmds
 
 try:
-	with open("config/bot.json") as file:
+	with open("config.json") as file:
 		data = json.load(file)
-	with open("config/template.bot.json") as file:
+	with open("template.config.json") as file:
 		template = json.load(file)
 	if not set(data.keys()) == set(template.keys()) \
 	or any(value in data.values() for value in template.values()):
 		raise KeyError
 except (FileNotFoundError, json.decoder.JSONDecodeError, KeyError):
-	print("Please create the \"config/bot.json\" file based on the template in \"config/template.bot.json\"")
+	print("Please create the \"config.json\" file based on the template in \"template.config.json\"")
 	sys.exit()
 
-bot = cmds.InteractionBot(test_guilds=data["guilds"])
+bot = cmds.InteractionBot(test_guilds=data["guilds"] or None)
 bot.config = data
 for name in data["extensions"]:
 	bot.load_extension(f"extensions.{name}")
